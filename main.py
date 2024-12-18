6bf8b22dfaafeece6a396059f44d07ad238538e6
br3ign <alexsamm231@hotmail.com>
2024-12-18 17:19:54 -0400

    added functions: sort_on and report

--- main.py
+++ main.py
@@ -1,24 +1,16 @@
 book_path = "books/frankenstein.txt"
-
-
-def main():
-    with open(book_path) as f:
-        file_contents = f.read()
-    
-    #print(file_contents)
-    count_words(file_contents)
-    count_characters(file_contents)
+file_contents = ""
 
 def count_words(file):
     word_count = 0
     for word in file.split():
         word_count += 1
-    
-    #print(word_count)
+    return word_count
 
 def count_characters(file):
+    global char_counter
     char_dict = {}
-    counter = 0
+    
     for word in file.split():
         lowered_string = word.lower()
         for char in lowered_string:
@@ -27,8 +19,36 @@ def count_characters(file):
             else:
                 char_dict[char] = 1
     
-    print(char_dict)
+    return char_dict
 
+def sort_on(ch_dict):
+    return ch_dict["count"]
+
+def report():
+    word_count = count_words(file_contents)
+    char_counter = count_characters(file_contents)
+
+    character = []
+    for char in char_counter:
+        if char.isalpha():
+            character.append({"name":char,"count":char_counter[char]})
+    
+    character.sort(reverse=True, key=sort_on)
+
+    print(f"--- Begin report or {book_path} ---")
+    print(f"{word_count} words found in the document\n")
+
+    for c in character:
+        print(f"The '{c["name"]}' character was found {c["count"]} times.")
+    
+    print(f"--- End report ---")
+
+def main():
+    global file_contents
+    with open(book_path) as f:
+        file_contents = f.read()
+        f.close()
+    
+    report()
 
 main()
-
